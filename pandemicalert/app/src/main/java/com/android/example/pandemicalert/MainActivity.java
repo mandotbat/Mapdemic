5afe1a15e3657bede9a0dfa3ad2394888a72c1ab
Naman Maheshwari <naman1901@gmail.com>
2016-02-04 01:03:02 +0530

    maps

--- pandemicalert/app/src/main/java/com/android/example/pandemicalert/MainActivity.java
+++ pandemicalert/app/src/main/java/com/android/example/pandemicalert/MainActivity.java
@@ -1,23 +1,34 @@
 package com.android.example.pandemicalert;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
+    private TileOverlay heatmapOverlay;
+    final private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +38,41 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Pandemic");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_search);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                final CharSequence diseases[] = new CharSequence[] {"Dengue", "Malaria", "Typhoid", "STDs"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Pick a Disease");
+                builder.setItems(diseases, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        createHeatMap(diseases[which]);
+                    }
+                });
+                builder.show();
             }
         });
 
-        try {
-            if(map==null) {
-                map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab_center);
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(map!=null) {
+                    // Add a marker in Delhi and move the camera
+                    LatLng delhi = new LatLng(28.6139, 77.2090);
+                    //map.addMarker(new MarkerOptions().position(delhi).title("Marker in Sydney"));
+                    map.animateCamera(CameraUpdateFactory.newLatLng(delhi));
+                }
             }
-            map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-            Marker m = map.addMarker(new MarkerOptions().position(new LatLng(0.00,0.00)).title("Center of earth"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        });
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -68,4 +96,35 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map = googleMap;
+
+        // Add a marker in Delhi and move the camera
+        LatLng delhi = new LatLng(28.6139, 77.2090);
+        map.addMarker(new MarkerOptions().position(delhi).title("Marker in Sydney"));
+        map.animateCamera(CameraUpdateFactory.newLatLng(delhi));
+        createHeatMap("hi");
+    }
+
+    private void createHeatMap(CharSequence disease) {
+
+        if(heatmapOverlay!=null){
+            heatmapOverlay.remove();
+        }
+        ArrayList<LatLng> locations = new ArrayList<>();
+        Random r = new Random();
+
+        for(int i=0;i<2000;i++) {
+            LatLng temp = new LatLng(r.nextFloat()*30+10, r.nextFloat()*25+65);
+            locations.add(temp);
+        }
+
+        HeatmapTileProvider heatmapProvider;
+        heatmapProvider = new HeatmapTileProvider.Builder().data(locations).build();
+        heatmapOverlay = map.addTileOverlay(new TileOverlayOptions().tileProvider(heatmapProvider));
+    }
+
 }
